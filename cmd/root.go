51ba5bca1197feead3af7f33e77d4c1da267f308
hackercat <me@hackerc.at>
2021-01-15 04:42:10 +0000

    feat: Add config support, add survey for default image
    Adds better config support via viper library and default image survey

--- cmd/root.go
+++ cmd/root.go
@@ -16,11 +16,17 @@ import (
 	gitignore "github.com/sabhiram/go-gitignore"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/nektos/act/pkg/artifacts"
 	"github.com/nektos/act/pkg/common"
 	"github.com/nektos/act/pkg/model"
 	"github.com/nektos/act/pkg/runner"
+)
+
+var (
+	Config  *viper.Viper
+	cfgFile string
 )
 
 // Execute is the entry point to running the CLI
@@ -236,24 +242,6 @@ func newRunCommand(ctx context.Context, input *Input) func(*cobra.Command, []str
 			return err
 		}
 
-		// Check if platforms flag is set, if not, run default image survey
-		if len(input.platforms) == 0 {
-			cfgFound := false
-			cfgLocations := configLocations()
-			for _, v := range cfgLocations {
-				_, err := os.Stat(v)
-				if os.IsExist(err) {
-					cfgFound = true
-				}
-			}
-			if !cfgFound && len(cfgLocations) > 0 {
-				if err := defaultImageSurvey(cfgLocations[0]); err != nil {
-					log.Fatal(err)
-				}
-				input.platforms = readArgsFile(cfgLocations[0])
-			}
-		}
-
 		// run the plan
 		config := &runner.Config{
 			Actor:                 input.actor,
@@ -304,49 +292,6 @@ func newRunCommand(ctx context.Context, input *Input) func(*cobra.Command, []str
 	}
 }
 
-func defaultImageSurvey(actrc string) error {
-	var answer string
-	confirmation := &survey.Select{
-		Message: "Please choose the default image you want to use with act:\n\n  - Large size image: +20GB Docker image, includes almost all tools used on GitHub Actions (IMPORTANT: currently only ubuntu-18.04 platform is available)\n  - Medium size image: ~500MB, includes only necessary tools to bootstrap actions and aims to be compatible with all actions\n  - Micro size image: <200MB, contains only NodeJS required to bootstrap actions, doesn't work with all actions\n\nDefault image and other options can be changed manually in ~/.actrc (please refer to https://github.com/nektos/act#configuration for additional information about file structure)",
-		Help:    "If you want to know why act asks you that, please go to https://github.com/nektos/act/issues/107",
-		Default: "Medium",
-		Options: []string{"Large", "Medium", "Micro"},
-	}
-
-	err := survey.AskOne(confirmation, &answer)
-	if err != nil {
-		return err
-	}
-
-	var option string
-	switch answer {
-	case "Large":
-		option = "-P ubuntu-latest=ghcr.io/catthehacker/ubuntu:full-latest\n-P ubuntu-latest=ghcr.io/catthehacker/ubuntu:full-20.04\n-P ubuntu-18.04=ghcr.io/catthehacker/ubuntu:full-18.04\n"
-	case "Medium":
-		option = "-P ubuntu-latest=ghcr.io/catthehacker/ubuntu:act-latest\n-P ubuntu-20.04=ghcr.io/catthehacker/ubuntu:act-20.04\n-P ubuntu-18.04=ghcr.io/catthehacker/ubuntu:act-18.04\n"
-	case "Micro":
-		option = "-P ubuntu-latest=node:12-buster-slim\n-P ubuntu-20.04=node:12-buster-slim\n-P ubuntu-18.04=node:12-buster-slim\n"
-	}
-
-	f, err := os.Create(actrc)
-	if err != nil {
-		return err
-	}
-
-	_, err = f.WriteString(option)
-	if err != nil {
-		_ = f.Close()
-		return err
-	}
-
-	err = f.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func watchAndRun(ctx context.Context, fn common.Executor) error {
 	recurse := true
 	checkIntervalInSeconds := 2
@@ -389,4 +334,79 @@ func watchAndRun(ctx context.Context, fn common.Executor) error {
 	<-ctx.Done()
 	folderWatcher.Stop()
 	return err
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	Config = viper.NewWithOptions(viper.KeyDelimiter(`::`))
+
+	if cfgFile != "" {
+		Config.SetConfigFile(cfgFile)
+	} else {
+		Config.AddConfigPath(filepath.Join(os.Getenv("HOME"), ".config", "act"))
+		Config.AddConfigPath(".")
+		Config.SetConfigType("yaml")
+		Config.SetConfigName("config")
+	}
+
+	Config.AutomaticEnv()
+
+	if err := Config.ReadInConfig(); err == nil {
+		log.Printf("Config: %s", Config.ConfigFileUsed())
+	} else {
+		cfgPath := filepath.Join(os.Getenv("HOME"), ".config", "act")
+		log.Debugf("Config not found. Creating...")
+
+		if err := os.MkdirAll(cfgPath, 0644); err != nil {
+			log.Error(err)
+		}
+		cfgPath = filepath.Join(cfgPath, "config.yaml")
+		Config.SetConfigFile(cfgPath)
+		if err := Config.SafeWriteConfig(); err != nil {
+			log.Fatal(err)
+		}
+
+		answer := ""
+		confirmation := &survey.Select{
+			Message: "Please choose the default image you want to use with act:\n\n  - Large size image: +20GB Docker image, includes almost all tools used on GitHub Actions\n  - Medium size image: ~1GB, includes only necessary tools to bootstrap actions and aims to be compatible with all actions\n  - Micro size image: <200MB, contains only NodeJS required to bootstrap actions, doesn't work with all actions\n\nDefault image and other options can be changed manually in: " + cfgPath,
+			Help:    "If you want to know why act asks you that, please go to https://github.com/nektos/act/issues/107",
+			Default: "Medium",
+			Options: []string{"Large", "Medium", "Micro"},
+		}
+
+		err = survey.AskOne(confirmation, &answer)
+		if err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
+
+		switch answer {
+		case "Large":
+			Config.Set(`images`, map[string]string{
+				`ubuntu-latest`: `ghcr.io/catthehacker/ubuntu:full-latest`,
+				`ubuntu-20.04`:  `ghcr.io/catthehacker/ubuntu:full-20.04`,
+				`ubuntu-18.04`:  `ghcr.io/catthehacker/ubuntu:full-18.04`,
+			})
+		case "Medium":
+			Config.Set(`images`, map[string]string{
+				`ubuntu-latest`: `ghcr.io/catthehacker/ubuntu:act-latest`,
+				`ubuntu-20.04`:  `ghcr.io/catthehacker/ubuntu:act-20.04`,
+				`ubuntu-18.04`:  `ghcr.io/catthehacker/ubuntu:act-18.04`,
+				`ubuntu-16.04`:  `ghcr.io/catthehacker/ubuntu:act-16.04`,
+			})
+		case "Micro":
+			Config.Set(`images`, map[string]string{
+				`ubuntu-latest`: `node:12-buster-slim`,
+				`ubuntu-20.04`:  `node:12-buster-slim`,
+				`ubuntu-18.04`:  `node:12-buster-slim`,
+				`ubuntu-16.04`:  `node:12-stretch-slim`,
+			})
+		}
+		if err := Config.WriteConfig(); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/pflag v1.0.5
+	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
 	github.com/xanzy/ssh-agent v0.3.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
